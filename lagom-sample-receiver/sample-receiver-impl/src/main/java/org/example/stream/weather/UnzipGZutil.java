a2ea906d80fcef8b8eb27b2caaac2270c63c5888
Mani <mani@test.com>
2018-06-19 10:48:39 +0530

    updated Gzip

--- lagom-sample-receiver/sample-receiver-impl/src/main/java/org/example/stream/weather/UnzipGZutil.java
+++ lagom-sample-receiver/sample-receiver-impl/src/main/java/org/example/stream/weather/UnzipGZutil.java
@@ -4,8 +4,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.util.zip.GZIPInputStream;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,7 +18,7 @@ public class UnzipGZutil {
 	private static final Logger LOGGER = LoggerFactory.getLogger(UnzipGZutil.class);
 
 	private static final String INPUT_GZIP_FILE = "C:\\Users\\sundaramoorthim\\Desktop\\junk\\test.gz";
-	private static final String OUTPUT_FILE = "C:\\\\Users\\\\sundaramoorthim\\\\Desktop\\\\junk\\\\test.txt";
+	private static final String OUTPUT_FILE = "C:\\Users\\sundaramoorthim\\Desktop\\\\junk\\test.txt";
 
 	public static void main(String[] args) {
 		UnzipGZutil.gunzipIt(INPUT_GZIP_FILE, OUTPUT_FILE);
@@ -27,19 +32,25 @@ public class UnzipGZutil {
 		LOGGER.info("inputFile :: {}", inputFile);
 		LOGGER.info("outputFile :: {}", outputFile);
 
-		byte[] buffer = new byte[1024];
 		LOGGER.info("UNZIP started ");
 
 		try {
 
 			GZIPInputStream gzis = new GZIPInputStream(new FileInputStream(inputFile));
-
 			FileOutputStream out = new FileOutputStream(outputFile);
 
-			int len;
-			while ((len = gzis.read(buffer)) > 0) {
-				out.write(buffer, 0, len);
+			ReadableByteChannel rbc = Channels.newChannel(gzis);
+			WritableByteChannel wbc = Channels.newChannel(out);
+			ByteBuffer buffer = ByteBuffer.allocate(65536);
+
+			while (rbc.read(buffer) != -1) {
+				buffer.flip();
+				wbc.write(buffer);
+				buffer.clear();
 			}
+
+			rbc.close();
+			wbc.close();
 
 			gzis.close();
 			out.close();
@@ -54,8 +65,9 @@ public class UnzipGZutil {
 				LOGGER.info("Delete operation is failed.");
 			}
 
-		} catch (IOException ex) {
-			ex.printStackTrace();
+		} catch (IOException e) {
+
+			LOGGER.error("Exception : " + ExceptionUtils.getStackTrace(e));
 		}
 		return "UNZIP completed";
 	}
